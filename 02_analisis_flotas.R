d5ce7880ef8aa806f85080788947d35f53aa2e6e
pmtempone <pmtempone@gmail.com>
2016-09-08 16:58:59 -0300

    ultimo analisis

--- 02_analisis_flotas.R
+++ 02_analisis_flotas.R
@@ -5,24 +5,11 @@ library(funModeling)
 library(Rcpp)
 library(ggplot2)
 
----#carga de datos----
 
-trenes.despachados <- read.csv2("E:/R_proyectos/Subte/trenes-despachados.csv",stringsAsFactors = FALSE)
-subte.estado.flota <- read.csv("E:/GitHub/Subtes/subte-estado-flota.csv", sep=";")
-
-str(trenes.despachados)
-summary(trenes.despachados)
-df_status(trenes.despachados)
-
-head(as.Date(trenes.despachados$FR1_FECHA,format = "%d/%m/%Y"))
-
-trenes.despachados$FR1_FECHA <- as.Date(trenes.despachados$FR1_FECHA,format = "%d/%m/%Y")
-
-----#analisis estado flota----
-
-subte.estado.flota$anio_mes <-  subte.estado.flota$ANIO*100+subte.estado.flota$MES
-
-df_status(subte.estado.flota)
+---#analisis---
+  
+  
+  df_status(subte.estado.flota)
 
 ggplot(data = subte.estado.flota,mapping = aes(x=anio_mes,y=EN_SERVICIO,group=1))+geom_line()
 
